1da5d915b979db77a01cf43c7db0b85868753a60
dimirhouse <dimirhouse@gmail.com>
2018-03-21 21:30:24 +0100

    made refactor

--- src/main/java/pl/dimirhouse/ItemInfo.java
+++ src/main/java/pl/dimirhouse/ItemInfo.java
@@ -1,7 +1,5 @@
 package pl.dimirhouse;
 
-import java.math.BigDecimal;
-
 public class ItemInfo {
 
     private String title;
@@ -13,28 +11,28 @@ public class ItemInfo {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getPrice() {
         return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
     }
 
     public String getPicture() {
         return picture;
     }
 
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-
     public String getUrl() {
         return url;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
     }
 
     public void setUrl(String url) {
